39fdc96b176e8da488a6378ec32fef88e7b3290c
VLVSV <40785126+VLVSV@users.noreply.github.com>
2018-07-03 00:51:45 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,157 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const sql = require("sqlite");
+sql.open("./score.sqlite");
+var ids = new Array();
+
+
+const prefix = "-";
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.channel.type !== "text") return;
+  if (message.channel.name !== "internal_scrim") return;
+
+  sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [message.author.id, 0]);
+    } 
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [message.author.id, 0]);
+    });
+  });
+
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "info")) {
+    const info = "-report @A @B: report a match where A won and B lost\n-points @A: check how many points A has\n-teampoints: check how many points the team has in total";
+    message.channel.send(info);
+  }
+
+  if (message.content.startsWith(prefix + "points")) { 
+    if(message.mentions.members.first() == undefined) {
+      message.channel.send("S-sorry, I don't know who that is.");
+      return;
+    }
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.mentions.members.first().user.id}"`).then(row => {
+      if (!row) {
+        message.channel.send("I-I'm sorry, " + message.mentions.members.first().user.username + " doesn't have any points yet... did I do something wrong?");
+        return;
+      }
+      if(row.points === 0) {
+        message.channel.send("I-I'm sorry, " + message.mentions.members.first().user.username + " doesn't have any points yet... did I do something wrong?");
+        return;
+      }
+      message.channel.send("Sugoi! " + message.mentions.members.first().user.username + " currently has " + row.points + " points! Keep up the good work!");
+    });
+  }
+
+  if (message.content.startsWith(prefix + "teampoints")) {
+    var sum = 0;
+    
+    for(var i = 0; i < ids.length; i++) {
+      
+      sql.get(`SELECT * FROM scores WHERE userId ="${ids[i]}"`).then(row => {
+        sum += row.points;
+        console.log(sum);
+      });
+    }
+    setTimeout(() => {
+      console.log(sum);
+        message.channel.send("Wow! You guys already have " + sum + " points! Keep up the good work!");
+    }, 2500);
+    
+  }
+
+  if (message.content.startsWith(prefix + "lewd")) {
+    message.channel.send("Hi, I'm Chris Hansen from Dateline NBC. Why don't you have a seat over there?", {files: ["https://i.imgur.com/2o9hNUv.jpg"]});
+    return;
+  }
+
+  /*if (message.content.startsWith(prefix + "leaderboard")) {
+    var top = new Array();
+    top.push(ids[0]);
+
+    for(var i = 1; i < ids.length; i++) {
+      if(top.length < 5) {
+        var insert = 0;
+        for(var j = 0; j < top.length; j++) {
+          
+        }
+      }
+    }
+  }*/
+
+  if (message.content.startsWith(prefix + "reset")) {
+    if(message.author.id === "261678719511429120") {
+      for(var i = 0; i < ids.length; i++) {
+        sql.run(`UPDATE scores SET points = 0 WHERE userId = ${ids[i]}`);
+      }
+      message.channel.send("I did it! I reset the scores!");
+    }
+    else {
+      message.channel.send("I'm sorry, VLV said that only he could do this... is there anything else I can do for you?");
+    }
+  }
+
+  if (message.content.startsWith(prefix + "report")) {
+    var firstAt = message.content.indexOf("!");
+    var firstClose = message.content.indexOf(">");
+    var lastAt = message.content.lastIndexOf("!");
+    var lastClose = message.content.lastIndexOf(">");
+    if(firstAt === lastAt) {
+      message.channel.send("S-sorry, but the correct format is -report @A @B where A is the winner and B is the loser. I'm not smart enough to read other formats... will you forgive me?");
+      return;
+    }
+    var winnerID = message.content.substring(firstAt + 1, firstClose);
+    var loserID = message.content.substring(lastAt + 1, lastClose);
+    if(winnerID === loserID) {
+      message.channel.send("I c-can't let you report a match against yourself, o-or Law says he's g-going to punish me.");
+      return;
+    }
+    console.log(message.content);
+    var winner = client.users.get(winnerID);
+    var loser = client.users.get(loserID);
+    console.log(winner.username);
+    console.log(loser.username);
+
+    sql.get(`SELECT * FROM scores WHERE userId ="${winner.id}"`).then(row => {
+      if (!row) {
+        sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [winner.id, 0]);
+      } 
+      else {
+        sql.run(`UPDATE scores SET points = ${row.points + 5} WHERE userId = ${winner.id}`);
+        if(ids.indexOf(winner.id) === -1) {
+          ids.push(winner.id);
+        }
+      }
+    }).catch(() => {
+      console.error;
+      sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER)").then(() => {
+        sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [winner.id, 0]);
+      });
+    });
+
+    sql.get(`SELECT * FROM scores WHERE userId ="${loser.id}"`).then(row => {
+      if (!row) {
+        sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [loser.id, 0]);
+      } 
+      else {
+        sql.run(`UPDATE scores SET points = ${row.points + 3} WHERE userId = ${loser.id}`);
+        if(ids.indexOf(loser.id) === -1) {
+          ids.push(loser.id);
+        }
+      }
+    }).catch(() => {
+      console.error;
+      sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER)").then(() => {
+        sql.run("INSERT INTO scores (userId, points) VALUES (?, ?)", [loser.id, 0]);
+      });
+    });
+
+    message.channel.send("Good job " + winner.username + "! You can do it next time " + loser.username + ", I believe in you!");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
